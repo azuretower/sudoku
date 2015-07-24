ced095b1d7fb43694e8710d3be220afb95c4c4f0
azuretower <ryan.merrill215@gmail.com>
2015-07-23 21:21:04 -0600

    adjusted some indexing issues with the current_puzzle map

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import os
 
+# I will be useing functions called chr() and ord()
+# ord() returns the ascii value of the character passed to it
+# chr() reverses ord() and returns the ascii chracter of the number passed to it
+
+# setting up the default sudoku values
 row_A = {'1': -1, '2': -1, '3': -1, '4': -1, '5': -1, '6': -1, '7': -1, '8': -1, '9': -1}
 row_B = {'1': -1, '2': -1, '3': -1, '4': -1, '5': -1, '6': -1, '7': -1, '8': -1, '9': -1}
 row_C = {'1': -1, '2': -1, '3': -1, '4': -1, '5': -1, '6': -1, '7': -1, '8': -1, '9': -1}
@@ -10,24 +15,33 @@ row_F = {'1': -1, '2': -1, '3': -1, '4': -1, '5': -1, '6': -1, '7': -1, '8': -1,
 row_G = {'1': -1, '2': -1, '3': -1, '4': -1, '5': -1, '6': -1, '7': -1, '8': -1, '9': -1}
 row_H = {'1': -1, '2': -1, '3': -1, '4': -1, '5': -1, '6': -1, '7': -1, '8': -1, '9': -1}
 row_I = {'1': -1, '2': -1, '3': -1, '4': -1, '5': -1, '6': -1, '7': -1, '8': -1, '9': -1}
+current_puzzle = {'A': row_A,
+                  'B': row_B,
+                  'C': row_C,
+                  'D': row_D,
+                  'E': row_E,
+                  'F': row_F,
+                  'G': row_G,
+                  'H': row_H,
+                  'I': row_I}
 
-current_puzzle = {'1': row_A, '2': row_B, '3': row_C, '4': row_D, '5': row_E, '6': row_F, '7': row_G, '8': row_H, '9': row_I}
 
-
+# printing out the current status of the puzzle
 def print_puzzle():
     letters = 'ABCDEFGHI'
-    print '   1 2 3 | 4 5 6 | 7 8 9'
+    print '    1 2 3 | 4 5 6 | 7 8 9'
     print ''
     temp = ''
     for i in range(1, 10):
         temp = ''
-        temp += letters[i - 1] + '  '
+        temp += letters[i - 1] + '   '
         if i is 4:
-            print '   - - - + - - - + - - -'
+            print '    - - - + - - - + - - -'
         if i is 7:
-            print '   - - - + - - - + - - -'
+            print '    - - - + - - - + - - -'
         for j in range(1, 10):
-            if current_puzzle[str(i)][str(j)] is -1:
+            # chr(i+64) will return A through I, see top of file
+            if current_puzzle[chr(i + 64)][str(j)] is -1:
                 temp += 'X '
             else:
                 temp += str(current_puzzle[str(i)][str(j)]) + ' '
@@ -38,24 +52,46 @@ def print_puzzle():
         print temp
 
 
-# Main menu loop
-os.system('clear')
-while True:
-
+def print_menu():
     print ''
-    print '1. Add Number'
-    print '2. Delete Number'
+    print '1. Enter Number'
+    print '2. Remove Number'
     print '3. Quit'
+    print ''
+    print status_message
 
-    choice = raw_input(': ')
+# clears the terminal window
+os.system('clear')
+status_message = 'Welcome to Sudoku cli Edition!'
+
+# Main program loop
+while True:
+    print_puzzle()
+    print_menu()
+    choice = raw_input(':')
+
+    # Enter a number
     if choice is '1':
+        status_message = 'Enter Location as Letter Number pair and the number ie: A1 1'
+        error = False
         os.system('clear')
-        print current_puzzle['A']['A1']
+        print_puzzle()
+        print ''
+        print status_message
+        number = raw_input(':')
+        try:
+            location, number = number.split()
+        except ValueError, e:
+            status_message = 'Incorrect input'
+            error = True
+
+        os.system('clear')
+        if not error:
+            status_message = ''
 
     elif choice is '2':
         os.system('clear')
-        print_puzzle()
-
+        print ord('A')
 
     elif choice is '3':
         os.system('clear')
@@ -64,4 +100,4 @@ while True:
 
     else:
         os.system('clear')
-        'Choice is not Valid'
+        status_message = 'Choice is not Valid'
